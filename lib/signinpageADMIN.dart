f4837ce46133ad8bc1c73dad4b3da4539b6eca6c
omarhilall <omar1910068@miuegypt.edu.eg>
2023-01-11 20:40:02 +0200

    fixing signinPage (Admin)

--- lib/signinpageADMIN.dart
+++ lib/signinpageADMIN.dart
@@ -1,13 +1,64 @@
-// ignore: file_names
 import 'package:flutter/material.dart';
+import 'constants.dart';
+
+const Color black = Color(0xFF000000);
 
 class signinAdmin extends StatelessWidget {
   const signinAdmin({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Center(child: Text('Enter Your ID')),
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text("Sign In(Admin)"),
+          centerTitle: true,
+          backgroundColor: kSecondaryColor),
+      body: ListView(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const Text("Please Enter your ID:"),
+              const SizedBox(width: 35),
+              Expanded(
+                child: SizedBox(
+                  width: 150,
+                  child: TextFormField(
+                    decoration: const InputDecoration(),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: const [],
+          ),
+          ButtonBar(
+            alignment: MainAxisAlignment.spaceBetween,
+            buttonPadding: const EdgeInsets.symmetric(
+              horizontal: 30,
+            ),
+            children: [
+              TextButton(
+                style: TextButton.styleFrom(
+                    foregroundColor: Colors.black,
+                    elevation: 2,
+                    backgroundColor: kPrimaryColor),
+                onPressed: () {},
+                child: const Text("Cancel"),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                    foregroundColor: Colors.black,
+                    elevation: 2,
+                    backgroundColor: kPrimaryColor),
+                onPressed: () {},
+                child: const Text("Submit"),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
